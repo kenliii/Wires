0c4adf2de357b76f94f89269c6fcaef68a1f1f63
kenliii <90875261+kenliii@users.noreply.github.com>
2023-08-12 01:21:24 -0700

    back fin

--- index.js
+++ index.js
@@ -33,3 +33,17 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage })
+
+
+//connect to mongodb database
+
+const PORT = process.env.PORT || 6001;
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => {
+    console.log(`${error} did not connect`);
+});
